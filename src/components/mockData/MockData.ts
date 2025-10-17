b695c24cb3f331513e12727c66a5cee83e15792a
sunshine-web-coder <marybarra440@gmail.com>
2025-10-17 22:18:28 +0100

    first commit2

--- src/components/mockData/MockData.ts
+++ src/components/mockData/MockData.ts
@@ -193,10 +193,8 @@ export const mockAccounts: Account[] = [
       fullName: 'Martin Kraft',
       email: 'mart***944@gmail.com',
       dateCreated: '10/17/2025', // m/d/y
-      username: 'l',
-      password: 'l'
-      // username: 'martinkraft1944',
-      // password: 'h@12345'
+      username: 'martinkraft1944',
+      password: 'h@12345'
     },
     bank_details: {
       account_type: 'Checking',
